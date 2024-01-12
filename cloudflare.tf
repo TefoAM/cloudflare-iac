93b70e37e40f783ccf7fda2c0b98e8d9c0bb46ac
TefoAM <tefopsalm37@gmail.com>
2024-01-12 15:33:48 +0200

    Step 2 - Initial commit with webserver definition.

--- cloudflare.tf
+++ cloudflare.tf
@@ -6,3 +6,49 @@ terraform {
     }
   }
 }
+resource "cloudflare_record" "terraform_managed_resource_f9bf3529ef83572efab975d717e630c6" {
+  name    = "tefo.life"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "13.248.243.5"
+  zone_id = "c6df5ea3259930bb26618693600e7e3d"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_659113188270238bdf9cd1178a5abeeb" {
+  name    = "tefo.life"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "76.223.105.230"
+  zone_id = "c6df5ea3259930bb26618693600e7e3d"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_76c628ddd39af5d9fec31b267875624c" {
+  comment = "NO idea what this is??!"
+  name    = "_domainconnect"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "_domainconnect.gd.domaincontrol.com"
+  zone_id = "c6df5ea3259930bb26618693600e7e3d"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_1c96563dfc98df4ab3583f93b1bba7b1" {
+  name    = "www"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "tefo.life"
+  zone_id = "c6df5ea3259930bb26618693600e7e3d"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_7f25c5be93e366e4bf8ec23307657a2b" {
+  name    = "tefo.life"
+  proxied = false
+  ttl     = 1
+  type    = "TXT"
+  value   = "\"MS=ms73782001\""
+  zone_id = "c6df5ea3259930bb26618693600e7e3d"
+}
+
