491104ef225e85e10a0d7a7c20c47978752810a1
tefo.motaung <tefo.motaung@investec.co.za>
2024-01-18 09:00:12 +0200

    fix: change image resizing tag

--- cloudflare.tf
+++ cloudflare.tf
@@ -67,12 +67,81 @@ resource "cloudflare_record" "terraform_managed_resource_7f25c5be93e366e4bf8ec23
 }
 
 
-resource "cloudflare_zone_settings_override" "example-com-settings" {
-  zone_id = cloudflare_record.terraform_managed_resource_f9bf3529ef83572efab975d717e630c6.zone_id
+# resource "cloudflare_zone_settings_override" "example-com-settings" {
+#   zone_id = cloudflare_record.terraform_managed_resource_f9bf3529ef83572efab975d717e630c6.zone_id
 
-  settings {
-    tls_1_3                  = "on"
-    automatic_https_rewrites = "on"
-    ssl                      = "strict"
-  }
+#   settings {
+#     tls_1_3                  = "on"
+#     automatic_https_rewrites = "on"
+#     ssl                      = "strict"
+#   }
+# }
+
+resource "cloudflare_zone_settings_override" "example-com-settings" {
+    zone_id           = "c6df5ea3259930bb26618693600e7e3d"
+    settings {
+        always_online               = "off"
+        always_use_https            = "on"
+        automatic_https_rewrites    = "on"
+        binary_ast                  = "off"
+        brotli                      = "on"
+        browser_cache_ttl           = 14400
+        browser_check               = "on"
+        cache_level                 = "aggressive"
+        challenge_ttl               = 1800
+        ciphers                     = []
+        cname_flattening            = "flatten_at_root"
+        development_mode            = "off"
+        early_hints                 = "off"
+        email_obfuscation           = "on"
+        filter_logs_to_cloudflare   = "off"
+        fonts                       = "off"
+        h2_prioritization           = "off"
+        hotlink_protection          = "off"
+        http2                       = "on"
+        http3                       = "on"
+        ip_geolocation              = "on"
+        ipv6                        = "on"
+        log_to_cloudflare           = "on"
+        max_upload                  = 100
+        min_tls_version             = "1.0"
+        mirage                      = "off"
+        opportunistic_encryption    = "on"
+        opportunistic_onion         = "on"
+        orange_to_orange            = "off"
+        origin_error_page_pass_thru = "off"
+        origin_max_http_version     = "2"
+        polish                      = "off"
+        prefetch_preload            = "off"
+        privacy_pass                = "on"
+        proxy_read_timeout          = "100"
+        pseudo_ipv4                 = "off"
+        response_buffering          = "off"
+        rocket_loader               = "off"
+        security_level              = "medium"
+        server_side_exclude         = "on"
+        sort_query_string_for_cache = "off"
+        ssl                         = "full"
+        tls_1_3                     = "on"
+        tls_client_auth             = "off"
+        true_client_ip_header       = "off"
+        visitor_ip                  = "on"
+        waf                         = "off"
+        websockets                  = "on"
+        zero_rtt                    = "off"
+
+        minify {
+            css  = "off"
+            html = "off"
+            js   = "off"
+        }
+
+        security_header {
+            enabled            = false
+            include_subdomains = false
+            max_age            = 0
+            nosniff            = false
+            preload            = false
+        }
+    }
 }
