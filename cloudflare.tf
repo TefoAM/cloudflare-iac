8ab0cf2cc5b6237fa9ff9152cb5489b74482042b
tefo.motaung <tefo.motaung@investec.co.za>
2024-01-18 11:59:30 +0200

    removed godaddy cname

--- cloudflare.tf
+++ cloudflare.tf
@@ -38,15 +38,15 @@ resource "cloudflare_record" "terraform_managed_resource_659113188270238bdf9cd11
   zone_id = "c6df5ea3259930bb26618693600e7e3d"
 }
 
-resource "cloudflare_record" "terraform_managed_resource_76c628ddd39af5d9fec31b267875624c" {
-  comment = "NO idea what this is??!"
-  name    = "_domainconnect"
-  proxied = true
-  ttl     = 1
-  type    = "CNAME"
-  value   = "_domainconnect.gd.domaincontrol.com"
-  zone_id = "c6df5ea3259930bb26618693600e7e3d"
-}
+# resource "cloudflare_record" "terraform_managed_resource_76c628ddd39af5d9fec31b267875624c" {
+#   comment = "NO idea what this is??!"
+#   name    = "_domainconnect"
+#   proxied = true
+#   ttl     = 1
+#   type    = "CNAME"
+#   value   = "_domainconnect.gd.domaincontrol.com"
+#   zone_id = "c6df5ea3259930bb26618693600e7e3d"
+# }
 
 
 resource "cloudflare_record" "tefoam_github_io_profile" {
