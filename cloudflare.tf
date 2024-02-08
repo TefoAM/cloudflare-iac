895ff28aa553636d78dc479609fa8589c85cccd9
tefo.motaung <tefo.motaung@investec.co.za>
2024-02-08 19:32:42 +0200

    added cloudflare pages config

--- cloudflare.tf
+++ cloudflare.tf
@@ -4,8 +4,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4"
     }
-
-
   }
 
   backend "remote" {
@@ -13,42 +11,11 @@ terraform {
     workspaces {
       name = "cloudflare-api-github"
     }
-
   }
-
 }
 provider "cloudflare" {
   api_token = "iZKSeN_T0ESSFH0bQijl1su-Uvuh64lznAURk_b4"
 }
-
-# resource "cloudflare_record" "terraform_managed_resource_f9bf3529ef83572efab975d717e630c6" {
-#   name    = "tefo.life"
-#   proxied = true
-#   ttl     = 1
-#   type    = "A"
-#   value   = "13.248.243.5"
-#   zone_id = "c6df5ea3259930bb26618693600e7e3d"
-# }
-
-# resource "cloudflare_record" "terraform_managed_resource_659113188270238bdf9cd1178a5abeeb" {
-#   name    = "tefo.life"
-#   proxied = true
-#   ttl     = 1
-#   type    = "A"
-#   value   = "tefoam.github.io"
-#   zone_id = "c6df5ea3259930bb26618693600e7e3d"
-# }
-
-# resource "cloudflare_record" "terraform_managed_resource_76c628ddd39af5d9fec31b267875624c" {
-#   comment = "NO idea what this is??!"
-#   name    = "_domainconnect"
-#   proxied = true
-#   ttl     = 1
-#   type    = "CNAME"
-#   value   = "_domainconnect.gd.domaincontrol.com"
-#   zone_id = "c6df5ea3259930bb26618693600e7e3d"
-# }
-
 
 resource "cloudflare_record" "tefoam_github_io_profile" {
   comment = "This is from github"
@@ -78,3 +45,23 @@ resource "cloudflare_record" "terraform_managed_resource_7f25c5be93e366e4bf8ec23
   zone_id = "c6df5ea3259930bb26618693600e7e3d"
 }
 
+# Pages project managing project source
+resource "cloudflare_pages_project" "source_config" {
+  account_id        = "57cfd03a9dc176b07c4a598dc25c844f"
+  name              = "profile"
+  production_branch = "main"
+  source {
+    type = "github"
+    config {
+      owner                         = "TefoAM"
+      repo_name                     = "cloudflare-iac"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "custom"
+      preview_branch_includes       = ["dev", "preview"]
+      preview_branch_excludes       = ["main", "prod"]
+    }
+  }
+}
